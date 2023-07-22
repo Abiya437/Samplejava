12b53bb29156763001f358c6cd19d5d1fa4e1cce
Abiya437 <abiyacm774@gmail.com>
2023-07-22 11:39:22 +0530

    first commit

---
+++ app1/src/app1/Register.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app1;
+
+import javax.swing.JFrame;
+
+public class Register
+{
+    
+    JFrame f1= new JFrame("Register");
+    
+    public void run(){
+        f1.setBounds(10,10, 500, 500);
+        f1.setVisible(true);
+        
+    }
+    
+    
+}
